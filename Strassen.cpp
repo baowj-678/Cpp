bbfe3816e1fafb1a8b629da3a2d765134a1e4f52
vilily <2047123316@qq.com>
2020-03-09 22:22:20 +0800

    'strassen'

---
+++ Strassen.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <malloc.h>
+using namespace::std;
+
+double** matrix_mul(double** A, double** B, int n);
+void print(double** M, int n);
+
+int main()
+{
+	double A[8][8];
+	double B[8][8];
+	for (int i(0); i < 8; i++)
+	{
+		for (int j(0); j < 8; j++)
+		{
+			A[i][j] = B[i][j] = 1;
+		}
+	}
+	print((double**)A, 8);
+}
+
+double** matrix_mul(double** A, double** B, int n)
+{
+	if (n == 0)
+		return 0;
+	double* row = new double[n];
+	double sum = 0;
+	for (int i(0); i < n; i++)
+	{
+		for (int j(0); j < n; j++)
+		{
+			sum = 0;
+			for (int k(0); k < n; k++)
+			{
+				sum += A[i][k] * B[k][j];
+			}
+			row[j] = sum;
+		}
+		for (int k(0); k < n; k++)
+		{
+			A[i][k] = row[k];
+		}
+	}
+	return A;
+}
+
+void print(double** M, int n)
+{
+	for (int i(0); i < n; i++)
+	{
+		for (int j(0); j < n; j++)
+		{
+			cout << M[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
