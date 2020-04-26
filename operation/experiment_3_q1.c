92036dbfd4b2c1c54527c03ec8739e8255464596
Vilily <2047123316@qq.com>
2020-04-26 22:30:06 +0800

    finish

--- operation/experiment_3_q1.c
+++ operation/experiment_3_q1.c
@@ -5,9 +5,14 @@
 #include <signal.h>
 #include <time.h>
 #include <stdlib.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
 
-int number;
-int is_empty = 1;
+struct Msg
+{
+    char is_empty;
+    char number;
+};
 
 void handler_kill(int signum)
 {
@@ -16,55 +21,60 @@ void handler_kill(int signum)
 
 void handler_print(int signum)
 {
-    // printf("hello");
-    if(is_empty == 0)
+    key_t key = ftok(".", 5);
+	int shmid = shmget(key, 0, 0);//通过ftok函数得到id值存在key中
+	void *pshm = shmat(shmid, 0, 0);//挂接操作，成功返回指向共享存储段的指针，出错返回-1
+    if (*(int *)pshm == -1) {//查看挂接是否成功如果出错返回-1，报错
+		printf("shmat error!\n");
+		exit(0);
+	}
+    struct Msg * msg = (struct Msg*)pshm;
+    if(msg->is_empty == 0)
     {
-        printf("\n\n\n\n\n%d  ", number);
-        is_empty = 1;
+        printf("%d\n", msg->number);
+        msg->is_empty = 1;
     }
 }
 int main()
 {
+    void *pshm;
+    key_t key = ftok(".", 5);
+    int shmid = shmget(key, sizeof(struct Msg), 0666 | IPC_CREAT);
+    pshm = shmat(shmid, 0, 0);
+    if (*(int *)pshm == -1) {   //查看挂接是否成功如果出错返回-1，报错
+		printf("shmat error!\n");
+		exit(0);
+	}
+    struct Msg *msg = (struct Msg *)pshm;
+    msg->is_empty = 1;
     pid_t pid;
+    signal(SIGUSR1, handler_print);
+    signal(SIGUSR2, handler_kill);
     pid = fork();
     //parent process
     if(pid != 0)
     {
-        printf("parent");
-        // int i;
-        // for(i = 1; i <= 100; i++)
-        // {
-        //     if(is_empty != 0)
-        //     {
-        //         number = i;
-        //         is_empty = 0;
-        // kill(pid, SIGUSR1);
-                // sleep(1);
-        //     }
-        //     if(i == 100)
-        //     {
-        //         kill(pid, SIGUSR2);
-        //         sleep(10);
-        //     }
-        // }
-        sleep(300);
-        wait(NULL);
+        int i;
+        for(i = 1; i <= 100; i++)
+        {
+            if(msg->is_empty != 0)
+            {
+                msg->number = i;
+                msg->is_empty = 0;
+                raise(SIGUSR1);
+                usleep(100000);
+            }
+            if(i == 100)
+            {
+                kill(pid, SIGUSR2);
+                sleep(1);
+            }
+        }
     }
     //subprocess
     if(pid == 0)
     {
-        // signal(SIGUSR1, handler_print);
-        // signal(SIGUSR2, handler_kill);
-        printf("\nhello");
-        int i = 0;
-        for(; i < 100; i++)
-        {
-            printf("hello");
-            sleep(1);
-        }
-        // int i=0;
-        // for(;i<100;i++)
-        // {}
+        sleep(100);
     }
     return 0;
 }
